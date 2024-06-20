be1c22de7e1c4d07f825158b0e7fd9ff65918c78
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-06-20 19:05:09 +0200

    Added zespol page with some mock data and placeholders for images.

--- src/app/zespol/page.js
+++ src/app/zespol/page.js
@@ -1,9 +1,27 @@
 import { Header } from '@/ui/components/Header'
+import { team } from '@/mock-data/team'
 
 export default function Zespol() {
   return (
     <>
       <Header title="Zespół" />
+      <div className="team-container max-w-[900px] w-full mt-6 mb-10 mx-auto">
+        {team.map((member) => (
+          <div className="team-member-container my-4">
+            <div className="team-member-header">
+              <h2 className="team-member-name text-2xl text-primary-900">{member.name}</h2>
+              <div className="separator relative bottom-3" />
+              <h3 className="team-member-title relative bottom-6 uppercase font-semibold text-sm text-gray-700 ">
+                {member.title}
+              </h3>
+            </div>
+            <div className="team-member-content flex flex-row">
+              <p className="team-member-description mr-10 text-justify ">{member.description}</p>
+              <div className="team-member-photo w-[360px] h-[480px] border border-secondary-200 shrink-0"></div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
---
+++ src/mock-data/team.js
@@ -0,0 +1,14 @@
+export const team = [
+  {
+    name: 'Judyta Ciesielska',
+    title: 'adwokat',
+    description:
+      'Erat stet ut lorem sed diam. Magna labore takimata sit elitr, tempor sed sadipscing amet et, eirmod kasd sadipscing gubergren sed no invidunt magna clita. Labore ut erat aliquyam diam et et. Eos gubergren sea dolores sadipscing. Clita amet sadipscing amet labore. Eirmod nonumy consetetur sed rebum kasd eos. Invidunt duo lorem dolore sit sanctus. Sit diam dolor nonumy lorem dolor at tempor dolor no. Clita et consetetur nonumy sit lorem eos, sea dolor labore magna no sed rebum dolor sed ea, sit lorem no duo invidunt, lorem eos eirmod vero labore ea dolore lorem est accusam. Elitr et elitr rebum ipsum, et no elitr invidunt et tempor rebum ipsum diam clita, et ea dolore invidunt vero et eirmod eirmod. Stet erat takimata dolor diam. Diam elitr vero voluptua dolor, est et stet tempor nonumy aliquyam amet accusam nonumy magna. Dolor kasd takimata justo kasd ea. Invidunt nonumy amet duo kasd rebum labore ipsum diam, ea elitr dolor erat aliquyam eirmod diam eos et, sed dolore lorem dolor duo sed amet takimata aliquyam diam. Sea sed vero rebum kasd, labore.',
+  },
+  {
+    name: 'Katarzyna Markiewicz',
+    title: 'adwokat',
+    description:
+      'Consetetur invidunt lorem labore ea amet. Voluptua stet gubergren sit sadipscing dolor lorem sed, diam et ea diam clita dolore, takimata et clita et et sit stet sit lorem, sit sit et accusam dolor sed takimata sed. Magna ipsum clita et aliquyam ea sed. Clita et vero duo ipsum amet dolore dolores aliquyam. Kasd diam sed eirmod no ipsum, clita dolores amet erat nonumy aliquyam ipsum ea sed. Invidunt dolor vero labore consetetur accusam justo est sadipscing duo. Diam amet kasd amet et dolores et ipsum vero diam, vero ipsum kasd lorem voluptua. Tempor dolore dolore amet erat dolores sea, no sit ipsum lorem magna ea sit dolor labore. Et eirmod lorem magna ipsum elitr accusam. Lorem ipsum ipsum dolor et diam eirmod. Diam dolores sed justo est et magna, diam dolores est ut dolor sed no aliquyam sea, duo. Labore amet ipsum consetetur aliquyam et ipsum diam. Sanctus sadipscing clita magna aliquyam eirmod kasd sed, magna elitr diam labore takimata accusam ipsum, at ut ipsum consetetur accusam at voluptua, est invidunt sanctus diam takimata et eirmod elitr et. Labore tempor ut ipsum et lorem ea vero, ipsum ea ipsum elitr amet eos amet justo sed accusam. Est sed ipsum sea ut sit.',
+  },
+]
