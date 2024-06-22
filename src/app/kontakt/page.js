dbd5089c7de5a24e57ec4b380fdee4ff9e75d339
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-06-22 23:16:49 +0200

    Added contact data to kontakt page.

--- src/app/kontakt/page.js
+++ src/app/kontakt/page.js
@@ -4,6 +4,35 @@ export default function Kontakt() {
   return (
     <>
       <Header title="Kontakt" />
+      <div className="contact-wrapper bg-primary-700">
+        <div className="contact-container flex flex-row max-w-[900px] mx-auto mt-8 mb-16 ">
+          {/* contact data */}
+          <div className="contact-data-container">
+            <div className="contact-address-container mt-3">
+              <div className="contact-address-icon relative top-6 right-9 w-6 h-6 border border-secondary-300 rounded-full"></div>
+              <h2 className="contact-address-label uppercase font-semibold text-white">Adres:</h2>
+              <p className="contact-text text-white text-sm">ul. Kancelarii 5/8</p>
+              <p className="contact-text text-white text-sm">62-200, Gniezno</p>
+            </div>
+            <div className="contact-email-container mt-3">
+              <div className="contact-email-icon relative top-6 right-9 w-6 h-6 border border-secondary-300 rounded-full"></div>
+              <h2 className="contact-email-label uppercase font-semibold text-sm text-white">
+                Email:
+              </h2>
+              <p className="contact-text text-white text-sm">kancelaria@example.com</p>
+            </div>
+            <div className="contact-phone-container mt-3">
+              <div className="contact-phone-icon relative top-6 right-9 w-6 h-6 border border-secondary-300 rounded-full"></div>
+              <h2 className="contact-phone-label uppercase font-semibold text-sm text-white">
+                Tel./Fax:
+              </h2>
+              <p className="contact-text text-white text-sm">kancelaria@example.com</p>
+            </div>
+          </div>
+
+          {/* contact form */}
+        </div>
+      </div>
     </>
   )
 }
