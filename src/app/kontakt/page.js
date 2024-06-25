8bebaae0dae407ad097416547409209a24d9fce4
MarcinOstrowskiCoding <marcin.ostrowski.coding@gmail.com>
2024-06-25 06:16:58 +0200

    Familiarizing myself with great iframe of reCAPTCHA v2 and its beautiful design that absolutely fits seamlessly to every possible layout.

--- src/app/kontakt/page.js
+++ src/app/kontakt/page.js
@@ -5,11 +5,16 @@ import AddressIco from '@/ui/icons/AddressIco'
 import EmailIco from '@/ui/icons/EmailIco'
 import PhoneIco from '@/ui/icons/PhoneIco'
 import Link from 'next/link'
-import { useState } from 'react'
+import Script from 'next/script'
+import { useEffect, useState } from 'react'
 
 export default function Kontakt() {
   const [formData, setFormData] = useState({})
   const [isRodoAccepted, setIsRodoAccepted] = useState(false)
+
+  useEffect(() => {
+    setFormData({ ...formData, jsEnabled: "yes"})
+  }, [])
 
   function onInputChange(event) {
     setFormData({ ...formData, [event.target.name]: event.target.value })
@@ -19,19 +24,61 @@ export default function Kontakt() {
     setIsRodoAccepted(event.target.checked)
   }
 
+  function validateForm() {
+    const { fullName, email, phone, topic, content, acceptRodo, name, jsEnabled } = formData
+    let message = ''
+    if (!fullName || !email || !phone || !topic || !content) {
+      message += 'Proszę wypełnić wszystkie pola oznaczone gwiazdką '
+    }
+
+    if (!validator.matches(fullName, /^[a-zA-Z\s]*$/)) {
+      message += 'Imie i nazwisko nie może zawierać cyfr i znaków specjalnych. '
+    }
+
+    if (!validator.isLength(fullName, { max: 50 })) {
+      message += 'Imię i nazwisko nie może być dłuższe niż 50 liter. '
+    }
+
+    if (!validator.isEmail(email)) {
+      message +='Proszę wprowadzić prawidłowy adres email. '
+    }
+
+    if (!validator.isMobilePhone(phone, 'any', { strictMode: false })) {
+      message += 'Prosze wprowadzić prawidłowy numer telefonu. '
+    }
+
+    if (!validator.isLength(topic, { max: 100 })) {
+      message += 'Temat nie może być dłuższy niż 100 znaków. '
+    }
+
+    if (!validator.isLength(content, { min:100, max: 1000 })) {
+      message += 'Wiadomość musi zawierać od 100 do 1000 znaków. '
+    }
+
+    if (acceptRodo !== "yes") {
+      message += "Aby wysłać wiadomość wymagana jest akceptacja warunków Polityki Prywatności"
+    }
+
+    if (jsEnabled !== "yes") return "bot detected"
+
+    if (name !== "") return "bot detected"
+  }
+
   const onSubmit = (event) => {
     event.preventDefault()
-    if (!isRodoAccepted) {
-      console.log('Please accept the RODO to proceed.')
-    } else {
-      console.log(formData)
-    }
+    validationMessage = validateForm()
+    if (validationMessage === 'bot detected') return
+    if (validationMessage !== "") return
+    console.log("form submission successful")
+
   }
 
   return (
     <>
+      <Script src="https://www.google.com/recaptcha/api.js" async defer />
+
       <Header title="Kontakt" />
-      <div className="contact-wrapper bg-primary-700" style={{ minHeight: 'calc(100vh - 380px)' }}>
+      <div className="contact-wrapper bg-primary-800" style={{ minHeight: 'calc(100vh - 380px)', backgroundColor: 'rgb (34 34 34)' }}>
         <div className="contact-container flex flex-row max-w-[900px] mx-auto mt-8 mb-16 ">
           {/* contact data */}
           <div className="contact-data-container pl-12 mt-12 min-w-[240px]">
@@ -61,20 +108,25 @@ export default function Kontakt() {
 
           {/* contact form */}
           <form className="contact-from-container flex flex-col min-w-[240px] w-full max-w-[680px] px-4 mt-8 mb-4 mx-auto">
-            <h2 className="contact-form-label p-1 mb-1 uppercase tracking-wide text-white font-semibold text-xl">{"formularz  kontaktowy"}</h2>
+            <h2 className="contact-form-label p-1 mb-1 uppercase tracking-wide text-white font-semibold text-xl">{"formularz kontaktowy"}</h2>
 
             <div className="contact-form-user-data grid grid-cols-1 md:grid-cols-2 gap-3 mb-3 ">
-              <input className="contact-input-name p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md" type="text" name="name" placeholder="Imię i nazwisko *" onChange={onInputChange} required />
+              <input className="contact-input-fullName p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md" type="text" name="fullName" placeholder="Imię i nazwisko *" onChange={onInputChange} required />
               <input className="contact-input-email p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md" type="text" name="email" placeholder="Email *" onChange={onInputChange} required />
               <input className="contact-input-phone p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md" type="text" name="phone" placeholder="Telefon *" onChange={onInputChange} required />
               <input className="contact-input-topic p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md" type="text" name="topic" placeholder="Temat *" onChange={onInputChange} required />
+
+              <input className="hidden" type="text" name="name" onChange={onInputChange} />
+              <input type="hidden" id="jsEnabled" name='jsEnabled' value="no" />
             </div>
 
-            <textarea className="contact-input-message h-48 p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md" type="text" name="message" placeholder="Wiadomość *" onChange={onInputChange} required />
+            <textarea className="contact-input-content h-48 p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md" type="text" name="content" placeholder="Wiadomość *" onChange={onInputChange} required />
 
+
+            <div className='oh-boy flex flex-row justify-between mt-6'>
             <div className="contact-accept-rodo flex flex-row mt-3">
-              <input className="accept-rodo-checkbox w-[18px]" type="checkbox" name="agree" value="yes" checked={isRodoAccepted} onChange={onCheckboxChange} />
-              <label className="accept-rodo-text ml-3 text-white font-thin" htmlFor="agree">
+              <input className="accept-rodo-checkbox w-[18px]" type="checkbox" name="acceptRodo" value="yes" checked={isRodoAccepted} onChange={onCheckboxChange} />
+              <label className="accept-rodo-text ml-3 text-white font-thin" htmlFor="acceptRodo">
                 Akceptuję
                 <Link className="link font-normal hover:text-secondary-200" href={'polityka-prywatnosci'} target="_blank" rel="noopener noreferrer">
                   {' Plitykę Prywantości'}
@@ -82,10 +134,16 @@ export default function Kontakt() {
                 i zasady przetwarzania danych osobowych RODO.
               </label>
             </div>
+            
+            <div className="g-recaptcha" data-sitekey={process.env.RECAPTCHA_SITE_KEY} data-theme="dark"></div>
+
+            </div>
 
             <button className="contact-button w-[160px] h-[38px] mt-3 ml-auto px-6 py-1 text-secondary-200 uppercase font-semibold border border-secondary-200 rounded-lg bg-primary-600 hover:bg-primary-500 hover:border-2" type="submit" onSubmit={onSubmit}>
               Wyślij
             </button>
+
+
           </form>
         </div>
       </div>
