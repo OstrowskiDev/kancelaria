cfb3bff1492bb2bbd93b7a420601d775be441533
MarcinOstrowskiCoding <marcin.ostrowski.coding@gmail.com>
2024-06-28 00:51:37 +0200

    Finalized Kontakt form error messages and their layout.

--- src/app/kontakt/page.js
+++ src/app/kontakt/page.js
@@ -1,6 +1,5 @@
 'use client'
 
-import { rodo } from '@/mock-data/rodo'
 import Checkmark from '@/ui/components/Checkmark'
 import { Header } from '@/ui/components/Header'
 import KontaktData from '@/ui/components/KontaktData'
@@ -12,7 +11,6 @@ import validator from 'validator'
 
 export default function Kontakt() {
   const [submitAttempted, setSubmitAttempted] = useState(false)
-  const [isRodoAccepted, setIsRodoAccepted] = useState(false)
   const [formData, setFormData] = useState({
     fullName: '',
     email: '',
@@ -40,18 +38,21 @@ export default function Kontakt() {
 
   useEffect(() => {
     validateForm()
-    console.log('filedValidity state in useEffect:', fieldValidity)
-    console.log(formData)
   }, [formData])
 
+  function printError(fieldName) {
+    return fieldValidity[fieldName].message.length > 0 && submitAttempted;
+  }
 
+  const printFullNameError = printError('fullName')
+  const printEmailError = printError('email')
+  const printPhoneError = printError('phone')
+  const printTopicError = printError('topic')
+  const printContentError = printError('content')
+  const printAcceptRodoError = printError('acceptRodo')
 
   function onInputChange(event) {
     setFormData({ ...formData, [event.target.name]: event.target.value })
-  }
-
-  function onCheckboxChange(event) {
-    setIsRodoAccepted(event.target.checked)
   }
 
   function validateForm() {
@@ -75,21 +76,10 @@ export default function Kontakt() {
       }
     }
 
-    const fields = {
-      fullName: 'imię i nazwisko',
-      email: 'email',
-      phone: 'telefon',
-      topic: 'temat',
-      content: 'wiadomość'
-    };
-
-    for (const [field, displayName ] of Object.entries(fields)) {
-      if (!formData[field]) {
-        addMessage(field, `pole ${displayName} jest wymagane`)
-      } else {
-        removeMessage(field, `pole ${displayName} jest wymagane`)
-      }
-    }
+    const fieldNames = ['fullName', 'email', 'phone', 'topic', 'content']
+    for (const field of fieldNames) {
+      validateField(field, () => !formData[field], `Pole wymagane.`)
+}
 
     validateField('fullName', () => !validator.matches(fullName, /^[a-zA-Z\s]*$/), 'Imie i nazwisko nie może zawierać cyfr i znaków specjalnych.')
 
@@ -111,11 +101,11 @@ export default function Kontakt() {
     validateField('name', () => (name !== ""), 'bot detected')
 
     setFieldValidity(validationResults)
-    console.log('validation results:', validationResults)
   }
 
   const onSubmit = (event) => {
     event.preventDefault()
+    setSubmitAttempted(true)
     const botDetected = fieldValidity.jsEnabled.message.length > 0 || fieldValidity.name.message.length > 0
     const hasValidationErrors = Object.values(fieldValidity).some(field => field.message.length > 0);
     if (botDetected) return
@@ -144,35 +134,52 @@ export default function Kontakt() {
             form-inputs-container' >
             <div className="contact-form-user-data grid grid-cols-1 md:grid-cols-2 gap-3 mb-3">
               <div className='contact-fullName-container flex flex-col'>
-                <input className={`contact-input-fullName p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${fieldValidity.fullName.message.length > 0 ? 'invalid-field' : ''}`} type="text" name="fullName" placeholder="Imię i nazwisko *" onChange={onInputChange} />
-                <label className='contact-label-fullName text-xs text-red-600' htmlFor="fullName" >{fieldValidity.fullName.message.join(' ')}</label>
+                <input className={`contact-input-fullName p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${printFullNameError && 'invalid-field'}`} type="text" name="fullName" placeholder="Imię i nazwisko *" onChange={onInputChange} />
+                <label className='contact-label-fullName text-xs text-red-600' htmlFor="fullName" >{printFullNameError && fieldValidity.fullName.message.join(' ')}</label>
               </div>
 
-              <input className={`contact-input-email p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${fieldValidity.email.message.length > 0 ? 'invalid-field' : ''}`} type="text" name="email" placeholder="Email *" onChange={onInputChange} />
+              <div className='contact-email-container flex flex-col'>
+                <input className={`contact-input-email p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${printEmailError && 'invalid-field'}`} type="text" name="email" placeholder="Email *" onChange={onInputChange} />
+                <label className='contact-label-email text-xs text-red-600' htmlFor="email" >{printEmailError && fieldValidity.email.message.join(' ')}</label>
+              </div>
 
-              <input className={`contact-input-phone p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${fieldValidity.phone.message.length > 0 ? 'invalid-field' : ''}`} type="text" name="phone" placeholder="Telefon *" onChange={onInputChange} />
+              <div className='contact-phone-container flex flex-col'>
+                <input className={`contact-input-phone p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${printPhoneError && 'invalid-field'}`} type="text" name="phone" placeholder="Telefon *" onChange={onInputChange} />
+                <label className='contact-label-phone text-xs text-red-600' htmlFor="phone" >{printPhoneError && fieldValidity.phone.message.join(' ')}</label>
+              </div>
 
-              <input className={`contact-input-topic p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${fieldValidity.topic.message.length > 0 ? 'invalid-field' : ''}`} type="text" name="topic" placeholder="Temat *" onChange={onInputChange} />
+              <div className='contact-topic-container flex flex-col'>
+                <input className={`contact-input-topic p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${printTopicError && 'invalid-field'}`} type="text" name="topic" placeholder="Temat *" onChange={onInputChange} />
+                <label className='contact-label-topic text-xs text-red-600' htmlFor="topic" >{printTopicError && fieldValidity.topic.message.join(' ')}</label>
+              </div>
 
               <input className="hidden" type="text" name="name" onChange={onInputChange} />
               <input type="hidden" id="jsEnabled" name='jsEnabled' value="no" />
             </div>
 
-            <textarea className="contact-input-content w-full h-48 p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md" type="text" name="content" placeholder="Wiadomość *" onChange={onInputChange} />
+            <div className='contact-content-container flex flex-col'>
+              <textarea className={`contact-input-content w-full h-48 p-[6px] border-2 border-primary-800 focus:border-secondary-200 focus:outline-none rounded-md ${printContentError && 'invalid-field'}`} type="text" name="content" placeholder="Wiadomość *" onChange={onInputChange} />
+              <label className='contact-label-content text-xs text-red-600' htmlFor="content" >{printContentError && fieldValidity.content.message.join(' ')}</label>
+            </div>
 
             <div className='contact-checkboxes-container flex flex-row justify-between mt-2'>
-            <div className="contact-accept-rodo flex flex-row items-center h-[76px] p-2 mr-4 bg-white border rounded-[4px]" style={{ borderColor: 'rgb(82, 82, 82)'}} >
-              <Checkmark setFormData={setFormData} formData={formData} />
-              <label className="accept-rodo-text max-w-[192px] ml-3 text-sm text-primary-800 leading-tight" htmlFor="acceptRodo">
-                {`Akceptuję `}
-                <Link className="link font-medium hover:text-primary-900 hover:border-b-2 hover:border-gray-600 " href={'polityka-prywatnosci'} target="_blank" rel="noopener noreferrer">
-                  {'Plitykę Prywantości'}
-                </Link>{' '}
-                i zasady przetwarzania danych osobowych RODO.
-              </label>
-              <div className='contact-rodo-icon w-[40px] h-[40px] ml-2'>
-              <RulesIco />
+            <div className='rodo-checkbox-container flex flex-col mr-4'>
+              <div className={`contact-accept-rodo flex flex-row items-center h-[76px] p-2  bg-white border-2 rounded-[4px] ${printAcceptRodoError && 'invalid-field'}`}
+              //  style={{ borderColor: 'rgb(82, 82, 82)'}} 
+               >
+                <Checkmark setFormData={setFormData} formData={formData} />
+                <label className="accept-rodo-text max-w-[192px] ml-3 text-sm text-primary-800 leading-tight" htmlFor="acceptRodo">
+                  {`Akceptuję `}
+                  <Link className="link font-medium hover:text-primary-900 hover:border-b-2 hover:border-gray-600 " href={'polityka-prywatnosci'} target="_blank" rel="noopener noreferrer">
+                    {'Plitykę Prywantości'}
+                  </Link>{' '}
+                  i zasady przetwarzania danych osobowych RODO.
+                </label>
+                <div className='contact-rodo-icon w-[40px] h-[40px] ml-2'>
+                <RulesIco />
+                </div>
               </div>
+              <label className='contact-label-rodo text-xs text-red-600' htmlFor="acceptRodo" >{printAcceptRodoError && fieldValidity.acceptRodo.message.join(' ')}</label>
             </div>
             
             <div className="g-recaptcha" data-sitekey={process.env.RECAPTCHA_SITE_KEY} data-theme="light"></div>
@@ -182,8 +189,6 @@ export default function Kontakt() {
             <button className="contact-button w-[160px] h-[38px] mt-3 ml-auto px-6 py-1 text-secondary-200 uppercase font-semibold border border-secondary-200 rounded-lg bg-primary-600 hover:bg-primary-500 hover:border-2" type="submit" >
               Wyślij
             </button>
-
-
           </form>
         </div>
       </div>
