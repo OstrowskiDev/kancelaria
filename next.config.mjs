acef4b0a0ba8ed7f335a32cb7d74e0529f936472
MarcinOstrowskiCoding <marcin.ostrowski.coding@gmail.com>
2024-07-02 11:36:38 +0200

    Added prettier config. Changed next.config.mjs to ensure that page will work both locally and on github pages.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
-// https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
-  basePath: '/kancelaria',
-  // assetPrefix: '/kancelaria/blob/gh-pages/',
-  assetPrefix: 'https://marcinostrowskicoding.github.io/kancelaria/',
+  basePath:  isProd ? '/kancelaria' : '',
+  assetPrefix: isProd ? '/kancelaria/': '' ,
+  // assetPrefix: 'https://marcinostrowskicoding.github.io/kancelaria/',
 }
 
 export default nextConfig
--- src/ui/components/Header.js
+++ src/ui/components/Header.js
@@ -1,16 +1,19 @@
 export function Header({ title }) {
+  console.log("Header")
   return (
     <div
       className="header flex justify-center items-center h-[240px] bg-primary-600"
       style={{
         backgroundImage: 'url("/header02small.jpg")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center 20%',
-        width: '100%',
+        backgroundSize: "cover",
+        backgroundPosition: "center 20%",
+        width: "100%",
       }}
     >
       <div className="w-full max-w-[900px] overflow-hidden">
-        <h1 className="uppercase text-5xl p-[6px] font-semibold text-white tracking-wider">{title}</h1>
+        <h1 className="uppercase text-5xl p-[6px] font-semibold text-white tracking-wider">
+          {title}
+        </h1>
       </div>
     </div>
   )
