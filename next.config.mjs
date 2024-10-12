f19fcf6cbf4d5b36bbe8404fdde93eb922368885
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-10-13 00:28:11 +0200

    Change next.config.mjs to be compatible with Firebase. Page will no longer be supported on GitHub Pages.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production"
+// const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/kancelaria" : "",
-  assetPrefix: isProd ? "/kancelaria/" : "",
   images: {
     unoptimized: true,
   },
