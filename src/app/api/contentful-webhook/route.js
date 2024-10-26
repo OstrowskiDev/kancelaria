cd645050f275e53c40deb39ee0f8854f34ea916b
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-10-26 20:21:55 +0200

    Added file execution logic to contentful-webhook endpoint so static files are generated every time content is updated.

--- src/app/api/contentful-webhook/route.js
+++ src/app/api/contentful-webhook/route.js
@@ -1,7 +1,30 @@
+const { exec } = require("child_process")
+
 export async function POST(request) {
   try {
     const body = await request.json()
-    console.log("Contentful Webhook Triggered:", body)
+    console.log("Contentful Webhook Triggered:")
+    if (body) {
+      console.log("Trying to run rebuild.sh...")
+
+      const inDevelopment = process.env.NODE_ENV === "development"
+      const command = inDevelopment
+        ? `cmd /c "src\\lib\\shell_scripts\\tests.bat"`
+        : `sh src/lib/shell_scripts/rebuild.sh`
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error executing script: ${error.message}`)
+          return
+        }
+
+        if (stderr) {
+          console.error(`Script stderr: ${stderr}`)
+          return
+        }
+
+        console.log(`Script output: ${stdout}`)
+      })
+    }
 
     return new Response(JSON.stringify({ message: "Webhook received" }), {
       headers: { "Content-Type": "application/json" },
@@ -16,3 +39,5 @@ export async function POST(request) {
     })
   }
 }
+
+// C:\VSC\project-32-kancelaria\kancelaria\src\lib\shell_scripts\rebuild.sh
