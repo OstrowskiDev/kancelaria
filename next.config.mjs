d6b01cc9aca1983b12edfe668b0b6b5a4b29b60c
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-11-10 00:39:06 +0100

    Removed no longer needed option output export from next.config.mjs.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-// const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
   },
