357b505040a412b4f5ebcee16ae3a226e1333175
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-12-01 17:55:32 +0100

    Expanded rate limiting to protect API endpoints based on IP and endpoint route.

--- src/lib/rateLimit.js
+++ src/lib/rateLimit.js
@@ -1,15 +1,33 @@
 import { RateLimiterMemory } from "rate-limiter-flexible"
 
-const rateLimiter = new RateLimiterMemory({
+// catch by IP address, max 10 requests per 10 minutes
+const rateLimiterByIp = new RateLimiterMemory({
   points: 2, // 2 points for testing, later will set to 10
-  duration: 15 * 60, // Per 15 minutes
+  duration: 10 * 60, // Per 10 minutes
+})
+
+//catch all, max 30 requests per minute
+const rateLimiterByEndpoint = new RateLimiterMemory({
+  points: 30,
+  duration: 60,
 })
 
 export async function applyRateLimit(req) {
   console.log("Applying rate limit")
   try {
-    const ip = req.headers.get("x-forwarded-for") || req.ip || "127.0.0.1"
-    await rateLimiter.consume(ip)
+    let ip
+    if (process.env.NODE_ENV === "development") {
+      ip = "127.0.0.1"
+    } else {
+      ip = req.headers.get("x-forwarded-for") || req.ip
+    }
+
+    if (!ip) {
+      throw new Error("IP address not found")
+    }
+
+    await rateLimiterByIp.consume(ip)
+    await rateLimiterByEndpoint.consume(req.url)
   } catch (rejRes) {
     throw new Error("Too many requests")
   }
