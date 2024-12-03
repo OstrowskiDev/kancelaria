ef493a4596d95103cbc10589c140976b59a4591e
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-12-03 15:50:47 +0100

    Improved layout and added some visual effect to fallback error page.

--- src/app/error.js
+++ src/app/error.js
@@ -1,10 +1,26 @@
 "use client"
 
-export default function Error({ error, reset }) {
+import { useEffect, useState } from "react"
+
+export default function Error() {
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+    setIsVisible(true)
+  }, [])
+
   return (
-    <div>
-      <h2>Przepraszamy, strona jest obecnie niedostępna</h2>
-      <p>Spróbuj później</p>
-    </div>
+    <mian
+      className={`error-handler flex items-center justify-center py-32 fade-in-500 ${isVisible && "make-visible"}`}
+      style={{ height: "calc(100vh - 80px - 60px)" }}
+    >
+      <div className="error-handler error-message-wrapper px-6 pt-16 pb-16 w-full bg-primary-700">
+        <h2 className="error-handler-title my-6 uppercase text-center text-white font-semibold text-2xl">
+          Strona jest obecnie niedostępna
+        </h2>
+      </div>
+    </mian>
   )
 }
+
+//
