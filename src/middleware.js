171c9a6b97960014cfd5d1bce545a502875f63d1
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-12-03 19:39:40 +0100

    Moved API global error handler directly to middleware.js file.

--- src/middleware.js
+++ src/middleware.js
@@ -4,14 +4,25 @@ import { applyRateLimit } from "@/lib/middleware/rateLimit"
 export async function middleware(req) {
   try {
     await applyRateLimit(req)
+
+    return NextResponse.next()
   } catch (error) {
+    if (error.message === "Rate limit exceeded") {
+      return NextResponse.json(
+        {
+          error:
+            "Too many requests were send recently, please try again later.",
+        },
+        { status: 429 },
+      )
+    }
+
+    console.error("Middleware Global Error Handler:", error)
     return NextResponse.json(
-      { error: "Too many requests from this IP, please try again later." },
-      { status: 429 },
+      { error: "Internal Server Error" },
+      { status: 500 },
     )
   }
-
-  return NextResponse.next()
 }
 
 export const config = {
