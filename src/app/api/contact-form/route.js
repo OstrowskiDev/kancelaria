91fa881db4bbd1060f63a698a4e6944ff19dd898
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2024-12-08 19:01:28 +0100

    Added sending emails functionality to form submission using SendGrid.

--- src/app/api/contact-form/route.js
+++ src/app/api/contact-form/route.js
@@ -1,5 +1,8 @@
 import { validateForm } from "@/lib/validators/contactFormValidation"
 import { NextResponse } from "next/server"
+import sendgrid from "@sendgrid/mail"
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY)
 
 export async function POST(req) {
   const formData = await req.json()
@@ -18,8 +21,28 @@ export async function POST(req) {
     )
   }
 
-  //!!! delete below line before production:
-  console.log("Form submitted with data: ", formData)
+  // send email via SendGrid
+  const title = "Testing SendGrid service"
+  const message = "This is a test message"
+
+  await sendgrid
+    .send({
+      to: process.env.SENDGRID_EMAIL_FROM,
+      from: process.env.SENDGRID_EMAIL_FROM,
+      subject: `${title}`,
+      html: `<p><strong>Test Message:</strong> ${message}</p>`,
+    })
+    .then(() => {
+      console.log("Email sent successfully")
+    })
+    .catch((error) => {
+      console.error(error)
+      return NextResponse.json(
+        { error: "Failed to send email" },
+        { status: 500 },
+      )
+    })
+
   return NextResponse.json(
     { message: "Form submitted successfully" },
     { status: 200 },
