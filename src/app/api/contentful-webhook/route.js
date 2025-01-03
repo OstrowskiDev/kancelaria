6305ff4d14d8367c9f509a04e81257583849cbb7
Marcin Ostrowski <marcin.ostrowski.coding@gmail.com>
2025-01-03 01:14:39 +0100

    Added new paths to revalidation triggered by Contentful webhook.

--- src/app/api/contentful-webhook/route.js
+++ src/app/api/contentful-webhook/route.js
@@ -3,6 +3,10 @@ import { revalidatePath } from "next/cache"
 export async function POST(request) {
   try {
     revalidatePath("/")
+    revalidatePath("/zespol")
+    revalidatePath("/zakres-uslug")
+    revalidatePath("/kontakt")
+    revalidatePath("/publikacje")
     return new Response(JSON.stringify({ message: "Webhook received" }), {
       headers: { "Content-Type": "application/json" },
       status: 200,
